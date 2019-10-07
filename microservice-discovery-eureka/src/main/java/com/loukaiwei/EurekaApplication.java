117ebb7a133aba954d823a86197cc7b0970efd5d
loukaiwei <lkw0831@126.com>
2019-10-07 11:48:59 +0800

    ffff

--- microservice-discovery-eureka/src/main/java/com/loukaiwei/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/loukaiwei/EurekaApplication.java
@@ -10,6 +10,5 @@ public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
-        System.out.println("EnableEurekaServer");
     }
 }
